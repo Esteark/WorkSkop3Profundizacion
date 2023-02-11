b4bb2e3521987a622a491c1286483380db298727
Esteark <eardila1409@gmail.com>
2023-02-11 15:02:36 -0500

    Add last Changes styles

--- src/components/galery/Galery.jsx
+++ src/components/galery/Galery.jsx
@@ -14,36 +14,36 @@ const Galery = () => {
             <h1 className="textCard">DEEP EARTH</h1>
           </figure>
 
-          <figure class="card2">
+          <figure className="card2">
             <h1 className="textCard">NIGHT ARCADE</h1>
           </figure>
 
-          <figure class="card3">
+          <figure className="card3">
             <h1 className="textCard">SOCCER TEAM VR</h1>
           </figure>
 
-          <figure class="card4">
+          <figure className="card4">
             <h1 className="textCard">THE GRID</h1>
           </figure>
 
-          <figure class="card5">
+          <figure className="card5">
             <h1 className="textCard">FROM UP ABOVE VR</h1>
           </figure>
 
-          <figure class="card6">
+          <figure className="card6">
             <h1 className="textCard">POCKET BOREALIS</h1>
           </figure>
 
-          <figure class="card7">
+          <figure className="card7">
             <h1 className="textCard">THE CURIOSITY</h1>
           </figure>
 
-          <figure class="card8">
+          <figure className="card8">
             <h1 className="textCard">MAKE IT FISHEYE</h1>
           </figure>
         </div>
 
-        <button class="button2">SEE ALL</button>
+        <button className="button2">SEE ALL</button>
       </section>
     </div>
   );
