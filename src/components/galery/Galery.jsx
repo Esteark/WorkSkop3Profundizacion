ef674323d53bab31e51e945797764c8822298099
Esteark <eardila1409@gmail.com>
2023-02-11 14:47:28 -0500

    Add corrections styles

--- src/components/galery/Galery.jsx
+++ src/components/galery/Galery.jsx
@@ -4,42 +4,42 @@ import "./galery.scss";
 const Galery = () => {
   return (
     <div>
-      <section class="section2">
-        <div class="section2txt">
+      <section className="section2">
+        <div className="section2txt">
           <h1>OUR CREATIONS</h1>
           <button class="button1">SEE ALL</button>
         </div>
         <div>
-          <figure class="card1">
-            <h1>DEEP EARTH</h1>
+          <figure className="card1">
+            <h1 className="textCard">DEEP EARTH</h1>
           </figure>
 
           <figure class="card2">
-            <h1>NIGHT ARCADE</h1>
+            <h1 className="textCard">NIGHT ARCADE</h1>
           </figure>
 
           <figure class="card3">
-            <h1>SOCCER TEAM VR</h1>
+            <h1 className="textCard">SOCCER TEAM VR</h1>
           </figure>
 
           <figure class="card4">
-            <h1>THE GRID</h1>
+            <h1 className="textCard">THE GRID</h1>
           </figure>
 
           <figure class="card5">
-            <h1>FROM UP ABOVE VR</h1>
+            <h1 className="textCard">FROM UP ABOVE VR</h1>
           </figure>
 
           <figure class="card6">
-            <h1>POCKET BOREALIS</h1>
+            <h1 className="textCard">POCKET BOREALIS</h1>
           </figure>
 
           <figure class="card7">
-            <h1>THE CURIOSITY</h1>
+            <h1 className="textCard">THE CURIOSITY</h1>
           </figure>
 
           <figure class="card8">
-            <h1>MAKE IT FISHEYE</h1>
+            <h1 className="textCard">MAKE IT FISHEYE</h1>
           </figure>
         </div>
 
